a31b9eecb2fb951089a9e57a5f023724548297fa
Paulo Vitor <paulovitorvaztst@gmail.com>
2024-10-23 19:43:58 -0300

    resolução do primeiro exercicio

--- src/exercicio1.java
+++ src/exercicio1.java
@@ -1,5 +1,8 @@
+//crie um programa Java que exiba a mensagem "olá mundo" no console. Em seguida, modifique o programa para exibir seu nome
+
 public class exercicio1 {
     public static void main(String[] args) {
         System.out.println("Olá mundo");
+        System.out.println("Olá Vitor! :)");
     }
 }

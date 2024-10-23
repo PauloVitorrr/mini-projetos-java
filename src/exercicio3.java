19e2d000289c0fc9ef9fdf6c7660379d953d455c
Paulo Vitor <paulovitorvaztst@gmail.com>
2024-10-23 19:58:48 -0300

    resolução do exercicio 3

---
+++ src/exercicio3.java
@@ -0,0 +1,13 @@
+//Crie um programa que declare e inicialize variáveis de todos os tipos primitivos em Java (int, double, char, boolean, etc.)
+// . Exiba o valor de cada variável no console.
+//
+
+public class exercicio3 {
+    public static void main(String[] args){
+       int inteiro = 5;
+       double variavel2 = 1.5;
+       String variavelTexto = "Olá mundo";
+       char variavelLetra1 = 'a';
+
+    }
+}

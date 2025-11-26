40bbcb4141a0f833fc429ea4e226cb8046673f43
Paulo Vitor <paulovitorvaztst@gmail.com>
2025-11-26 10:42:13 -0300

    exercicio 34 impar ou par

---
+++ src/exercicios/exercicio34.java
@@ -0,0 +1,19 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio34 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero para ver se ele é impar ou par");
+
+        int numero = scanner.nextInt();
+
+        if(numero % 2 == 0 ){
+            System.out.println("Este numero é par");
+        } else {
+            System.out.println("Este numero é impar");
+        }
+    }
+}

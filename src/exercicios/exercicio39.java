6b954a09455e0b6313f403a1461a3cdcf3c098b5
Paulo Vitor <paulovitorvaztst@gmail.com>
2025-11-27 16:21:38 -0300

    Exercicio 39

---
+++ src/exercicios/exercicio39.java
@@ -0,0 +1,25 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio39 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua idade");
+
+        int idade1 = scanner.nextInt();
+
+        System.out.println("Digite outra idade");
+
+        int idade2 = scanner.nextInt();
+
+        if(idade1 == idade2){
+            System.out.println("As idades são iguais");
+        } else if(idade1 > idade2){
+            System.out.println("A pessoa com a primeira idade é a mais velha");
+        } else {
+            System.out.println("A pessoa com a segunda idade é a mais velha");
+        }
+    }
+}

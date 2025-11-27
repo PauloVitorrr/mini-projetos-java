a247ded931ca298fb394fe4389db5a29d229bee7
Paulo Vitor <paulovitorvaztst@gmail.com>
2025-11-27 15:55:04 -0300

    Exercicio 38

---
+++ src/exercicios/exercicio38.java
@@ -0,0 +1,23 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio38 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite sua nota escolar de 0 a 10");
+
+        double nota = scanner.nextInt();
+
+        if(nota < 5){
+            System.out.println("Nota insuficiente");
+        } else if (nota >= 5 && nota <= 7){
+            System.out.println("Suficiente");
+        } else if (nota > 7 && nota <=10){
+            System.out.println("Boa nota");
+        } else {
+            System.out.println("Nota invalida");
+        }
+    }
+}

6cba1370420bdd277fff347c425ce86e4e12429f
Paulo Vitor <paulovitorvaztst@gmail.com>
2025-11-30 20:54:17 -0300

    exercicios 45,46,47,48,49 e 50

---
+++ src/exercicios/exercicio47.java
@@ -0,0 +1,9 @@
+package exercicios;
+
+public class exercicio47 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/exercicios/exercicio50.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio50 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero de 1 a 10 para tabuada");
+
+        int numero = scanner.nextInt();
+
+        for(int i = 0; i <= 10; i++){
+            int result = i * numero;
+            System.out.println(numero + "*" + i + "=" + result);
+        }
+    }
+}

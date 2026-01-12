e8d1f4979d4ce93f4342a9dade3892e9f5088bef
Paulo Vitor <paulovitorvaztst@gmail.com>
2026-01-12 16:32:52 -0300

    exercicio 57

---
+++ src/exercicios/exercicio57.java
@@ -0,0 +1,19 @@
+package exercicios;
+
+public class exercicio57 {
+    public static void main(String[] args) {
+        int impar = 0;
+        int par = 0;
+
+        for (int i = 0; i <= 100; i++){
+            if(i % 2 == 0){
+                par += i;
+            } else {
+                impar += i;
+            }
+        }
+
+        System.out.println("a soma dos impares " + impar);
+        System.out.println("a somda dos pares " + par);
+    }
+}

67ff05e47276c0d8cf9b9c4587a6a10867d4fcbd
Paulo Vitor <paulovitorvaztst@gmail.com>
2026-01-12 17:18:11 -0300

    exercicio 58

---
+++ src/exercicios/exercicio58.java
@@ -0,0 +1,19 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio58 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o numero de 1 a 10");
+
+        int numero = scanner.nextInt();
+        while (numero < 1 || numero > 10){
+            System.out.println("Número inválido. Tente novamente");
+            System.out.println("Digite um número de 1 e 10: ");
+            numero = scanner.nextInt();
+        }
+
+    }
+}

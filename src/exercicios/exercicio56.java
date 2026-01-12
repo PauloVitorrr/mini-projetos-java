8e14f7adf4f52657b045a89f60c613b27526ace4
Paulo Vitor <paulovitorvaztst@gmail.com>
2026-01-12 16:26:29 -0300

    exercicio 56

---
+++ src/exercicios/exercicio56.java
@@ -0,0 +1,13 @@
+package exercicios;
+
+public class exercicio56 {
+    public static void main(String[] args) {
+        int produto = 1;
+
+        for(int i = 1; i <= 10; i++){
+            produto *= i;
+        }
+
+        System.out.println("O produto de um número é " + produto);
+    }
+}

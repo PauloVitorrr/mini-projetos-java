e0d00e64b8bacc6e5b716167112e10c01c50caf4
Paulo Vitor <paulovitorvaztst@gmail.com>
2026-01-12 16:18:07 -0300

    exercicio 1 de array

---
+++ src/exercicios/array/exerciciosArrays.java
@@ -0,0 +1,16 @@
+package exercicios.array;
+
+import java.util.Arrays;
+
+public class exerciciosArrays {
+    public static void main(String[] args) {
+        //INVERTA OS ELEMENTOS DO ARRAY
+        int[] arrayOriginal = {1,2,3,4,5};
+        int[] arrayInvertido = new int[arrayOriginal.length];
+
+        for(int i = 0; i < arrayOriginal.length; i++){
+            arrayInvertido[i] = arrayOriginal[arrayOriginal.length - 1 -i];
+        }
+        System.out.println(Arrays.toString(arrayInvertido));
+    }
+}

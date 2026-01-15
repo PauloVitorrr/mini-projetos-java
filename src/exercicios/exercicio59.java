45e46932f3ca012c3103f0ea43e69acb35ecafa1
Paulo Vitor <paulovitorvaztst@gmail.com>
2026-01-15 13:19:32 -0300

    exercicio 59 loop de repetição

---
+++ src/exercicios/exercicio59.java
@@ -0,0 +1,23 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio59 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero para somar");
+
+        int numero = scanner.nextInt();
+
+        int somaAcumulativa = 0;
+
+        while (numero != 0){
+            somaAcumulativa = somaAcumulativa + numero;
+            System.out.println("Digite outro numero para somar ");
+            numero = scanner.nextInt();
+        }
+
+        System.out.println("A soma acumulativa é:" + somaAcumulativa);
+    }
+}

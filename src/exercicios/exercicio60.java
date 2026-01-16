5e4ffc9d897cfa9943e0f2c1bacd30030aae367c
Paulo Vitor <paulovitorvaztst@gmail.com>
2026-01-16 11:04:10 -0300

    exercicio 60 loop de repetição

---
+++ src/exercicios/exercicio60.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio60 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero positivo para contagem");
+        int numero = scanner.nextInt();
+        int contador = 0;
+
+        while(numero >= 0){
+            contador ++;
+            System.out.println("Digite novamente um numero positivo para contagem");
+            numero = scanner.nextInt();
+        }
+
+        System.out.println("Contador de numeros inteiros " + contador);
+    }
+}

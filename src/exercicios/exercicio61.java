b94ea4b91ca0ea85d01c8d179474f18c8605f863
Paulo Vitor <paulovitorvaztst@gmail.com>
2026-01-18 12:02:13 -0300

    exercicio 61 loop de repetição

---
+++ src/exercicios/exercicio61.java
@@ -0,0 +1,29 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio61 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro positivo: ");
+        int n = sc.nextInt();
+
+        int i = 0;
+
+        while (i * i <= n) {
+            i++;
+        }
+
+        int anterior = i - 1;
+
+        if ((n - (anterior * anterior)) < ((i * i) - n)) {
+            System.out.println("Raiz quadrada aproximada: " + anterior);
+        } else {
+            System.out.println("Raiz quadrada aproximada: " + i);
+        }
+
+        sc.close();
+
+    }
+}

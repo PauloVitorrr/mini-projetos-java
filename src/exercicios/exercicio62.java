abedfcec6643aadc8335fc88df5ed53613067498
Paulo Vitor <paulovitorvaztst@gmail.com>
2026-01-20 19:13:35 -0300

    exercicio 62 loop de repetição

---
+++ src/exercicios/exercicio62.java
@@ -0,0 +1,21 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio62 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite um numero");
+
+        int numero = scanner.nextInt();
+
+
+        while(numero <= 1000){
+           numero = numero * 2;
+           System.out.println("Valor da multiplicação é " + numero);
+        }
+
+        System.out.println("Valor final aós multiplicação acumulada: " + numero);
+    }
+}

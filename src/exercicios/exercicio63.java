a100dfd101f70a21b725c01c0738a4bbfd086181
Paulo Vitor <paulovitorvaztst@gmail.com>
2026-01-27 10:29:00 -0300

    exercicio 63 (do-while)

---
+++ src/exercicios/exercicio63.java
@@ -0,0 +1,22 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio63 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String senhaCorreta = "teste";
+        String senha;
+
+        do{
+            System.out.println("Digite a senha correta");
+            senha = scanner.nextLine();
+
+            if(!senha.equals(senhaCorreta)){
+                System.out.println("Senha incorreta digite novamente");
+            }
+        } while(!senha.equals(senhaCorreta));
+
+        System.out.println("Parabéns vc acertou a senha correta!");
+    }
+}
